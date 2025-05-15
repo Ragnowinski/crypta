f1a6d98aa801dfeb1c58f07c0242c3b1f5a04180
Niclas Chmill <niclas@worksimple.de>
2025-05-15 18:22:30 +0200

    Use dynamic base URL for GitHub Pages and custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+
 export default defineConfig({
-  base: '/crypta/',
+  base: isGitHubPages ? '/crypta/' : '/',
   plugins: [react()]
 })
