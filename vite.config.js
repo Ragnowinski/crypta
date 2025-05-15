42d17e649d9af19258b1afc0f7580854f1bbf49d
Niclas Chmill <niclas@worksimple.de>
2025-05-15 18:23:50 +0200

    Fix base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const isGitHubPages = process.env.GITHUB_PAGES === 'true';
-
 export default defineConfig({
-  base: isGitHubPages ? '/crypta/' : '/',
+  base: '/',
   plugins: [react()]
 })
